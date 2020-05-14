0c3a3f154128de6edfe1d5cb641a8b00c71647dd
yukidaminori <rinngo903@gmail.com>
2020-05-14 07:51:45 +0000

    change Table name

--- database/migrations/2020_05_12_162137_create_favorites_table.php
+++ database/migrations/2020_05_12_162137_create_favorites_table.php
@@ -13,7 +13,7 @@ class CreateFavoritesTable extends Migration
      */
     public function up()
     {
-        Schema::create('favorites', function (Blueprint $table) {
+        Schema::create('user_favorites', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
              $table->integer('micropost_id')->unsigned()->index();
@@ -35,6 +35,6 @@ class CreateFavoritesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('favorites');
+        Schema::dropIfExists('user_favorites');
     }
 }
